c2e74f0c501adaf1178486b725fb5f67303a383f
devhaute <devhaute@gmail.com>
2021-10-09 23:53:31 +0900

    초기 환경 세팅

---
+++ my-assets/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  my-assets
+//
+//  Created by devhaute on 2021/10/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ my-assets/my_assetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_assetsApp.swift
+//  my-assets
+//
+//  Created by devhaute on 2021/10/09.
+//
+
+import SwiftUI
+
+@main
+struct my_assetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
